8b72c372b990fe8a4b551fb7db73d22a87607903
Masaru Kobashigawa <masaru.kobashigawa@gmail.com>
2026-06-24 23:35:48 +0900

    Correct coverage expectations to 67 detailed cards

--- tests/test_note_context_ops.py
+++ tests/test_note_context_ops.py
@@ -48,11 +48,11 @@ class CoverageMappingTests(unittest.TestCase):
             {
                 "phase_01": 63,
                 "phase_02": 93,
-                "phase_03": 71,
+                "phase_03": 73,
                 "phase_04": 26,
                 "phase_05": 19,
                 "phase_06": 10,
-                "individual_card": 69,
+                "individual_card": 67,
             },
             dict(counts),
         )
